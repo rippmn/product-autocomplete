7580ffe9892bf4ac76f45640c9f5f021579d1e36
rippmn <rippmn@gmail.com>
2017-12-03 11:45:31 -0600

    add simple data persistence

--- src/main/java/com/rippmn/product/web/ProductSearchController.java
+++ src/main/java/com/rippmn/product/web/ProductSearchController.java
@@ -4,13 +4,20 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rippmn.product.domain.Product;
+import com.rippmn.product.persistence.ProductRepository;
 
 @RestController
 public class ProductSearchController {
 	
 	private static Logger log = LoggerFactory.getLogger(ProductSearchController.class); 
+	
+	@Autowired
+	ProductRepository productRepo;
 	
 	@RequestMapping("/")
 	public String hello() {
@@ -18,4 +25,10 @@ public class ProductSearchController {
 		return new Date().toString();
 	}
 
+	
+	@RequestMapping("/products")
+	public Iterable<Product> allProducts(){
+		return productRepo.findAll();
+	}
+	
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into Product values(1, '1111','Product 1');
+insert into Product values(2,'2222','Prod 2');
