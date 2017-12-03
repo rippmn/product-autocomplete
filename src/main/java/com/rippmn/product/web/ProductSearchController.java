9df080207843171f97d91e06594b13114c4d57c4
rippmn <rippmn@gmail.com>
2017-12-03 10:34:04 -0600

    add logging as we will need it later

--- src/main/java/com/rippmn/product/web/ProductSearchController.java
+++ src/main/java/com/rippmn/product/web/ProductSearchController.java
@@ -2,14 +2,19 @@ package com.rippmn.product.web;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductSearchController {
 	
+	private static Logger log = LoggerFactory.getLogger(ProductSearchController.class); 
+	
 	@RequestMapping("/")
 	public String hello() {
+		log.debug("returning homepage");
 		return new Date().toString();
 	}
 
