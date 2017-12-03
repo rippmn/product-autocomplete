890b0f3c2e25387f246e8f052ffad0c999442993
rippmn <rippmn@gmail.com>
2017-12-03 12:09:04 -0600

    add more data to test auto complete page

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
-insert into Product values(1, '1111','Product 1');
-insert into Product values(2,'2222','Prod 2');
+insert into Product(id, sku, name) values(1, '1111','Product 1');
+insert into Product(id, sku, name) values(2,'2222','Prod 2');
+insert into Product(id, sku, name) values(3, '3333','Something Else');
+insert into Product(id, sku, name) values(4, '4444','Another one');
