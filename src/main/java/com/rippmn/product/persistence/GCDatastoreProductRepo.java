f411efb91d7836676b21fe14bc1e6e7bc0bab95b
rippmn <rippmn@gmail.com>
2017-12-06 22:42:12 -0600

    replace deprecated methods

--- src/main/java/com/rippmn/product/persistence/GCDatastoreProductRepo.java
+++ src/main/java/com/rippmn/product/persistence/GCDatastoreProductRepo.java
@@ -9,6 +9,7 @@ import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PropertyProjection;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 
 @Repository
@@ -18,8 +19,6 @@ public class GCDatastoreProductRepo implements ProductRepository {
 	private DatastoreService datastore;
 	private static final String PRODUCT_NAME_KIND = "test";
 
-	private int prodCount=0;
-	
 	public GCDatastoreProductRepo() {
 	    datastore = DatastoreServiceFactory.getDatastoreService(); // Authorized Datastore service
 	  }
@@ -29,13 +28,12 @@ public class GCDatastoreProductRepo implements ProductRepository {
 		
 		Query query = new Query(PRODUCT_NAME_KIND);
 		query.addProjection(new PropertyProjection("name", String.class));
-		query.addFilter("name", FilterOperator.GREATER_THAN_OR_EQUAL, name).addFilter("name", FilterOperator.LESS_THAN, name+"{");
+		
+		query.setFilter(CompositeFilterOperator.and(FilterOperator.GREATER_THAN_OR_EQUAL.of("name", name), FilterOperator.LESS_THAN.of("name", name.concat("{"))));
 		return datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
 		
 	}
 	
-	//public Iterable<Entity> listProd
-
 	@Override
 	public String createProduct(String name, String sku) {
 		
