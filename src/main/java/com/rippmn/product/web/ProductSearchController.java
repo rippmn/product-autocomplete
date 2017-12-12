6e2c0c0754ca6bb368d2ae21e10cae898588c71c
rippmn <rippmn@gmail.com>
2017-12-11 21:38:47 -0600

    remove old testing data

--- src/main/java/com/rippmn/product/web/ProductSearchController.java
+++ src/main/java/com/rippmn/product/web/ProductSearchController.java
@@ -17,9 +17,6 @@ public class ProductSearchController {
 
 	private static Logger log = LoggerFactory.getLogger(ProductSearchController.class);
 
-	static final String[][] products = new String[][] 
-		{{"43900","Duracell - AAA Batteries (4-Pack)"},{"48530","Duracell - AA 1.5V CopperTop Batteries (4-Pack)"},{"127687","Duracell - AA Batteries (8-Pack)"},{"150115","Energizer - MAX Batteries AA (4-Pack)"},{"185230","Duracell - C Batteries (4-Pack)"},{"185267","Duracell - D Batteries (4-Pack)"},{"312290","Duracell - 9V Batteries (2-Pack)"},{"324884","Directed Electronics - Viper Audio Glass Break Sensor"},{"333179","Energizer - N Cell E90 Batteries (2-Pack)"},{"346575","Metra - Radio Installation Dash Kit for Most 1989-2000 Ford, Lincoln & Mercury Vehicles - Black"},{"346646","Metra - Radio Dash Multikit for Select GM Vehicles - Black"}};
-		
 	@Autowired
 	ProductRepository productRepo;
 
